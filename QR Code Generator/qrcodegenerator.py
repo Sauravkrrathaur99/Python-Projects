c7c312f95f38cf6c651033339fad941aa552df03
Saurav Kumar Rathaur <71010336+Sauravkrrathaur99@users.noreply.github.com>
2022-08-25 01:16:27 +0530

    SSnake game programmed using python and pygame

--- QR Code Generator/qrcodegenerator.py
+++ QR Code Generator/qrcodegenerator.py
@@ -6,7 +6,7 @@ qr = qrcode.QRCode(
     border=5
 )
 
-data = "https://www.linkedin.com/in/saurav-kumar-rathaur-14b3771a1/"
+data = "sunil kumar"
 qr.add_data(data)
 qr.make(fit=True)
 img = qr.make_image(fill="black",back_color = "white")
